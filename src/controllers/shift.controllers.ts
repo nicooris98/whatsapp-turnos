cece6c2dc33aee1d367b858721cd722552c20d10
Nicolas Ysidoro Oris <noris@siltium.com>
2023-03-08 12:40:58 -0300

    avances

--- src/controllers/shift.controllers.ts
+++ src/controllers/shift.controllers.ts
@@ -5,20 +5,14 @@ export const createShift = async (req: Request, res: Response) => {
   try {
     const shift = new Shift()
 
-    shift.date = req.body.date
-    shift.schedule = req.body.schedule
-    shift.firstName = req.body.firstName
-    shift.lastName = req.body.lastName
-    shift.observation = req.body.observation
-    shift.dni = req.body.dni
-    shift.social = req.body.social
+    Object.assign(shift, req.body)
 
     await shift.save()
 
     return res.json(shift)
   } catch (error) {
-    if(error instanceof Error)
-    return res.status(500).json({message: error.message})
+    if (error instanceof Error)
+      return res.status(500).json({ message: error.message })
   }
 }
 
@@ -28,7 +22,49 @@ export const getShifts = async (req: Request, res: Response) => {
 
     return res.json(shifts)
   } catch (error) {
-    if(error instanceof Error)
-    return res.status(500).json({message: error.message})
+    if (error instanceof Error)
+      return res.status(500).json({ message: error.message })
+  }
+}
+
+export const updateShift = async (req: Request, res: Response) => {
+  try {
+    const shift = await Shift.findOne({ where: { id: +req.params.id } })
+
+    if (!shift) return res.status(404).json({ message: "Shift does not exist" })
+
+    await Shift.update({id: +req.params.id}, req.body)
+
+    return res.sendStatus(204)
+  } catch (error) {
+    if (error instanceof Error)
+      return res.status(500).json({ message: error.message })
+  }
+}
+
+export const deleteShift = async (req: Request, res: Response) => {
+  try {
+    const shift = await Shift.findOne({ where: { id: +req.params.id } })
+
+    if (!shift) return res.status(404).json({ message: "Shift does not exist" })
+
+    await Shift.softRemove(shift)
+
+    return res.sendStatus(204)
+  } catch (error) {
+    if (error instanceof Error)
+      return res.status(500).json({ message: error.message })
+  }
+}
+
+export const getShift = async (req: Request, res: Response) => {
+  try {
+    const shift = await Shift.find({where: {id: +req.params.id}})
+
+    if (!shift) return res.status(404).json({ message: "Shift does not exist" })
+    return res.json(shift)
+  } catch (error) {
+    if (error instanceof Error)
+      return res.status(500).json({ message: error.message })
   }
 }
--- src/routes/shift.routes.ts
+++ src/routes/shift.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express"
-import { createShift, getShifts } from "../controllers/shift.controllers"
+import { createShift, deleteShift, getShifts, updateShift } from "../controllers/shift.controllers"
 
 const router = Router()
 
 router.post("/shift", createShift)
 
 router.get("/shifts", getShifts)
+
+router.put("/shift/:id", updateShift)
+
+router.delete("/shift/:id", deleteShift)
+
+router.get("/shift/:id", deleteShift)
 
 export default router
